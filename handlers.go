04df2d7ccc17b59b1d6d49d8f05d018770646957
SimplyLuckyy <gidsgreyman@gmail.com>
2025-12-17 10:51:37 -0600

    database

--- handlers.go
+++ handlers.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"net/http"
+	"encoding/json"
 	"fmt"
+	"log"
+	"strings"
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
@@ -37,4 +40,70 @@ func (cfg *apiConfig) handlerResetHits(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func handlerValidate(w http.ResponseWriter, r *http.Request) {
+	type params struct {
+		Body string `jason:"body"`
+	}
+	type vals struct {
+		CleanedBody string `json:"cleaned_body"`
+	} 
+
+	decoder := json.NewDecoder(r.Body)
+	chirp := params{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		errorResponse(w, http.StatusInternalServerError, "Couldn't decode params", err)
+		return
+	}
+	
+	const maxLength = 140
+	if len(chirp.Body) > maxLength {
+		errorResponse(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+
+	words := strings.Split(chirp.Body, " ")
+
+	for i, word := range words {
+		if (strings.ToLower(word) == "kerfuffle") {
+			words[i] = "****"
+		} else if (strings.ToLower(word) == "sharbert") {
+			words[i] = "****"
+		} else if (strings.ToLower(word) == "fornax") {
+			words[i] = "****"
+		}
+	}
+
+	cleaned := strings.Join(words, " ")
+
+	jsonResponse(w, http.StatusOK, vals{
+		CleanedBody: cleaned,
+	})
+}
+
+func errorResponse(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {log.Println(err)}
+	if code > 499 {log.Printf("Responding with 5XX eeror: %s", msg)}
+	
+	type errorJson struct {
+		Error string `json:"error"`
+	}
+	jsonResponse(w, code, errorJson{
+		Error: msg,
+	})
+}
+
+func jsonResponse(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(dat)
 }
--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"github.com/SimplyLuckyy/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"net/http"
 	"log"
 	"sync/atomic"
+	"os"
+	"database/sql"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
 	const rootPath = "."
 	const port = "8080"
+	dbURL := os.Getenv("DB_URL")
+	db, _ := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
 	
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db: dbQueries,
 	}
 
 	serveMux := http.NewServeMux()
@@ -23,6 +33,7 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", handlerReadiness)
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handlerServerHits)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerResetHits)
+	serveMux.HandleFunc("POST /api/validate_chirp", handlerValidate)
 
 	serverStruct := &http.Server{
 		Handler: serveMux,
